727f175549f787cfb3a3555dab1fc5a2a0590cb8
Sarah Allen <sarah@ultrasaurus.com>
2009-07-23 20:28:06 -0400

    categories and products source adapters are working, except for qparams

--- products.rb
+++ products.rb
@@ -1,6 +1,6 @@
 require 'lib/product_shop_inventory'
 
-class Categories < SourceAdapter
+class Products < SourceAdapter
   def initialize(source,credential)
     super(source,credential)
     @inventory = ProductShop::Inventory.new()
@@ -10,10 +10,11 @@ class Categories < SourceAdapter
   end
 
   def query
-    puts "=========================================== query "
+    puts "=========================================== Products query "
     @result = {}
+    puts "qparams=#{qparams}"
     begin
-      @result = @inventory.categories
+      @result = @inventory.products(1)
       puts @result.inspect
     rescue
       puts "*** ERROR ***"
@@ -24,19 +25,24 @@ class Categories < SourceAdapter
   end
 
   def sync
-    puts "=========================================== sync "
+    puts "=========================================== Products sync "
     super # this creates object value triples from an @result variable that contains an array of hashes
   end
 
   def create(name_value_list)
+    puts "=========================================== Products create "
     raise "not available"
   end
 
   def update(name_value_list)
+    puts "=========================================== Products update "
+
     raise "not available"
   end
 
   def delete(name_value_list)
+    puts "=========================================== Products delete "
+
     raise "not available"
   end
 
